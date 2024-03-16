0f38a2bc3ca64e7adee049fa953390bea57ac40e
zylaaamelll <122710143+zylaaamelll@users.noreply.github.com>
2024-03-16 08:11:42 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,15 +19,15 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'Bot Zylaa' //ur bot name
+global.ownernumber = '60167073321' //ur owner number
+global.ownername = '🦄 zyla' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
+global.packname = "Sticker By Bot Zylaa"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
